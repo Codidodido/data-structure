33c2af7ba2a64865351bf7caf08209912d29418d
Codidodido <139787880+Codidodido@users.noreply.github.com>
2024-01-31 03:58:19 +0330

    Modify student cpp
    - Add student structure

--- LinkedList/Student.cpp
+++ LinkedList/Student.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 using namespace std;
+
+struct Student{
+	string name;
+	int age;
+	int ID;
+	float mark;
+};
 int main(){
 	
 	cout << "-> Welcome To Codidodido School <-\n\n" << endl;
