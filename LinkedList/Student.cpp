9b594e7ac53dc0340534b6a7862bce3994ef8138
Codidodido <139787880+Codidodido@users.noreply.github.com>
2024-01-31 04:03:02 +0330

    Modify student cpp and add execute file
    - Add design and commands to work with program

--- LinkedList/Student.cpp
+++ LinkedList/Student.cpp
@@ -142,4 +142,94 @@ int main(){
 	
 	cout << "-> Welcome To Codidodido School <-\n\n" << endl;
 	
+	ClassList* computer = new ClassList();
+	
+	cout << "-> Order List: \n-> 1. Add Student\n-> 2. Remove Student\n-> 3. Edit Student Mark\n-> 4. Find Student\n-> 5. Print Student\n-> 6. Class Average\n-> 7. Class Variance\n-> 0. Exit\n";
+	int order = -1, data;
+	while(order != 0){
+		cout << "\n\n-> What to do : ";
+		cin >> order;
+		
+		switch(order){
+			
+			case 1:{
+				cout << "\n-> Enter number of students: ";
+				cin >> data;
+				for(int i = 0; i < data ; i++){
+					Student student;
+					cout << "\n\t-> Enter student ID: ";
+					cin >> student.ID;
+					cout << "\t-> Enter student name: ";
+					cin >> student.name;
+					cout << "\t-> Enter student age: ";
+					cin >> student.age;
+					cout << "\t-> Enter student mark: ";
+					cin >> student.mark;
+					
+					Node* newStudent = CreateStudent(student);
+					computer->AddStudent(newStudent);
+					cout << "\t-> Student Added Successfully!";
+				}
+				break;
+			}
+			
+			case 2:{
+				cout << "\n\t-> Enter student ID: ";
+				cin >> data;
+				computer->RemoveStudent(data);
+				cout << "\t-> Student Removed Successfully!";
+				break;
+			}
+			
+			
+			case 3:{
+				cout << "\n-> Enter student ID: ";
+				cin >> data;
+				
+				int mark;
+				cout << "\t-> Enter student mark: ";
+				cin >> mark;
+				
+				Node* student = computer->Find(data);
+				student->student.mark = mark;
+				cout << "\t-> Student Edited Successfully!";
+				break;
+			}
+			
+			case 4:{
+				cout << "\n-> Enter student ID: ";
+				cin >> data;
+				
+				Node* student = computer->Find(data);
+				computer->PrintStudent(student);
+				break;
+			}
+			
+			case 5: {
+				computer->Print();
+				break;
+			}
+			
+			case 6:{
+				float average = computer->GetAverage();
+				cout << "\n-> Class Average Mark is : " << average;
+				break;
+			}
+			
+			case 7:{
+				float variance = computer->GetVariance();
+				cout << "\n-> Class Variance Mark is : " << variance;
+				break;
+			}
+			
+			case 0: {
+				break;
+			}
+			
+			default:
+                cout << "Invalid input. Please try again." << endl;
+				
+		}
+		
+	}
 }
