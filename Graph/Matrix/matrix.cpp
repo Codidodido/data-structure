b3ad5abf6bd51be063cc1e34a7a59fe7e2e08603
Codidodido <139787880+Codidodido@users.noreply.github.com>
2024-01-31 04:59:27 +0330

    Modify matrix cpp and add execute file
    - Add command lines to run program

--- Graph/Matrix/matrix.cpp
+++ Graph/Matrix/matrix.cpp
@@ -67,4 +67,84 @@ class Graph{
 };
 
 int main(){
+	// v = vertices, e = edges
+	cout << "-> Welcome To Codidodido Graph <-\n\n";
+	
+	int v,e;
+	
+	cout << "-> Enter Graph Vertexes Number: ";
+	cin >> v;
+	
+	Graph g = Graph(v);
+	
+	// s = source, d = destination, w = weight
+	int s,d,w;
+	
+	int order = -1;
+	
+	cout << "-> Commands List:\n-> 1. Add Edge\n-> 2. Remove Edge\n-> 3. Find Edge\n-> 4. Find Min Edge\n-> 5. Print Graph\n-> 0. Exit\n";
+	
+	while(order != 0){
+		cout << "\n\n-> What to do: ";
+		cin >> order;
+		switch(order){
+			case 1:{
+				
+				cout << "\t-> Enter src vertex: ";
+				cin >> s;
+				cout << "\t-> Enter dest vertex: ";
+				cin >> d;
+				cout << "\t-> Enter src->dest weight: ";
+				cin >> w;
+				g.AddEdge(s,d,w);
+				cout << "\t-> Edge Added!";
+				break;
+			}
+			
+			case 2:{
+				
+				cout << "\t-> Enter src vertex: ";
+				cin >> s;
+				cout << "\t-> Enter dest vertex: ";
+				cin >> d;
+				g.RemoveEdge(s,d);
+				cout << "\t-> Edge Removed!";
+				break;
+			}
+			
+			case 3:{
+				
+				int i = g.FindMinEdge().firstIndex;
+				int j = g.FindMinEdge().secondIndex;
+				cout << "\t-> Min Edge Is Connection Between " << i << " -> " << j << " And Its Weight Is: " << g.Find(i,j);
+				break;
+			}
+				
+			case 4:{
+				
+				cout << "\t-> Enter src vertex: ";
+				cin >> s;
+				cout << "\t-> Enter dest vertex: ";
+				cin >> d;
+				w = g.Find(s,d);
+				cout << "\t-> Weight Of Edge Between " << s << " -> " << d << " Is: " << w;
+				break;
+			}
+			
+			case 5:{
+				
+				g.Print();
+				break;
+			}
+				
+			case 0:{
+				break;
+			}
+				
+			default:
+				cout << "Invalid Command";
+				
+						
+		}
+	}
 }
